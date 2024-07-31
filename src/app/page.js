3b3dd2af9cf8ce494d67ef3ba60962c391e48313
Babina081 <babitamoo333@gmail.com>
2024-07-31 21:48:53 +0545

    update commented other component

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Accordion from "@/components/Accordion";
 import InfScroll from "@/components/InfScroll";
+import Typo from "@/components/Typo";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <InfScroll></InfScroll>
-      <Accordion></Accordion>
+      <Typo></Typo>
+      {/* <InfScroll></InfScroll> */}
+      {/* <Accordion></Accordion> */}
     </main>
   );
 }
